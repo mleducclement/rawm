47f77a1fa16b8d3b7663320523149ca8f8744247
Schaezar <schaezar@hotmail.fr>
2025-03-31 13:33:50 -0400

    ReAdd game sorting functionalities + clean sort selector

--- src/components/SortSelector.tsx
+++ src/components/SortSelector.tsx
@@ -1,4 +1,4 @@
-﻿import { Button, Menu, MenuItem, Portal } from "@chakra-ui/react";
+﻿import { Button, Menu, Portal } from "@chakra-ui/react";
 import { BsChevronDown } from "react-icons/bs";
 
 interface SortOption {
@@ -8,41 +8,34 @@ interface SortOption {
 
 interface Props {
   selectedSortOrder: string;
-  onSelectSortOrder: (order: string) => void;
+  onSelectSortOrder: (sortOption: string) => void;
 }
 
 const SortSelector = ({ selectedSortOrder, onSelectSortOrder }: Props) => {
   const sortingOptions: SortOption[] = [
+    { display: "Relevance", value: "" },
     { display: "Name", value: "name" },
-    { display: "Date Released", value: "released" },
+    { display: "Date Released", value: "-released" },
     { display: "Date Added", value: "added" },
-    { display: "Date Created", value: "created" },
-    { display: "Date Updated", value: "updated" },
-    { display: "Rating", value: "rating" },
-    { display: "Metacritic Score", value: "metacritic" }
+    { display: "Rating", value: "-rating" },
+    { display: "Metacritic Score", value: "-metacritic" }
   ];
 
   const selectedOptionDisplay = selectedSortOrder ?
-    sortingOptions.find(opt => opt.value === selectedSortOrder)?.display : null;
+    sortingOptions.find(opt => opt.value === selectedSortOrder) : null;
 
   return (
     <Menu.Root>
       <Menu.Trigger asChild>
         <Button variant="outline" size="sm">
-          {selectedOptionDisplay && ("Order By: " + selectedOptionDisplay) || "Sort By:"}<BsChevronDown />
+          Ordered By: {selectedOptionDisplay?.display || "Relevance"}<BsChevronDown />
         </Button>
       </Menu.Trigger>
       <Portal>
         <Menu.Positioner>
           <Menu.Content>
-            <MenuItem>Relevance</MenuItem>
-            <MenuItem>Date Added</MenuItem>
-            <MenuItem>Name</MenuItem>
-            <MenuItem>Release Date</MenuItem>
-            <MenuItem>Popularity</MenuItem>
-            <MenuItem>Average Rating</MenuItem>
-            {/*{sortingOptions.map(opt => <Menu.Item key={opt.value} onClick={() => onSelectSortOrder(opt.value)}*/}
-            {/*                                      value={opt.value}>{opt.display}</Menu.Item>)}*/}
+            {sortingOptions.map(opt => <Menu.Item key={opt.value} onClick={() => onSelectSortOrder(opt.value)}
+                                                  value={opt.value}>{opt.display}</Menu.Item>)}
           </Menu.Content>
         </Menu.Positioner>
       </Portal>
