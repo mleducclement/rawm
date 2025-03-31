7d2a22f96ead681db68095a48127e6065552a6ef
Schaezar <schaezar@hotmail.fr>
2025-03-31 10:51:14 -0400

    Add selected genre highlight

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -3,10 +3,11 @@ import { HStack, Image, Link, List, Spinner } from "@chakra-ui/react";
 import getCroppedImageUrl from "@/services/image-url.ts";
 
 interface Props {
+  selectedGenre: Genre | null;
   onSelectGenre: (genre: Genre) => void;
 }
 
-const GenreList = ({ onSelectGenre }: Props) => {
+const GenreList = ({ selectedGenre, onSelectGenre }: Props) => {
   const { data, isLoading, error } = useGenres();
 
   if (error) return null;
@@ -19,7 +20,9 @@ const GenreList = ({ onSelectGenre }: Props) => {
         <List.Item key={genre.id} paddingY="5px" fontSize="lg">
           <HStack>
             <Image boxSize="32px" borderRadius={8} src={getCroppedImageUrl(genre.image_background)} />
-            <Link fontSize="lg" variant="plain" style={{ textDecoration: "none" }}
+            <Link fontSize="lg" fontWeight={genre.id === selectedGenre?.id ? "bold" : "normal"}
+                  variant="plain" padding={1}
+                  style={{ textDecoration: "none" }}
                   onClick={() => onSelectGenre(genre)}>{genre.name}</Link>
           </HStack>
         </List.Item>
