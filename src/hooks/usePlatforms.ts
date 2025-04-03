f92052719aee30a700b2dd9aacd52cdfead8536b
Schaezar <schaezar@hotmail.fr>
2025-04-03 14:38:15 -0400

    Change api endpoint to get parents platforms instead of platforms

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -14,7 +14,7 @@ const usePlatforms = () =>
   useQuery({
     queryKey: CACHE_KEY_PLATFORMS,
     queryFn: () => apiClient
-      .get<FetchResponse<Platform>>("/platforms")
+      .get<FetchResponse<Platform>>("/platforms/lists/parents")
       .then(res => res.data),
     staleTime: 24 * 60 * 60 * 1000,
     initialData: { count: platforms.length, results: platforms }
